ac04a2f6426804586880a5c38abd8e1eb3b5c6ce
Ravi Der <derravi00@gmail.com>
2026-01-12 22:17:29 +0530

    Add File Uploading Theory

--- Practice/Bank_Account_Management_API/accountent.py
+++ Practice/Bank_Account_Management_API/accountent.py
@@ -7,7 +7,7 @@ import json
 app = FastAPI(title="Bank Account Management API")
 
 class account_details(BaseModel):
-    acount_id : Annotated[str,Field(...,description="Enter the Account ID",examples=["A001"])]
+    account_id : Annotated[str,Field(...,description="Enter the Account ID",examples=["A001"])]
     name : Annotated[str,Field(...,description="Enter the name of the Accountent.",examples=["Harshil"])]
     balance : Annotated[float,Field(...,description="Enter the Balacne of the Account.",examples=[123.01])]
     gmail : Annotated[str,Field(...,description="Enter the Gmail of the Accountent.",examples=["abcd@gmail.com"])]
@@ -46,3 +46,19 @@ def account_details(account_no:str):
         raise HTTPException(status_code=402,detail="Data not Existed")
     
     return data[account_no]
+
+@app.post("/add_account")
+def add_account(account:account_details):
+
+    data = load_data()
+
+    if account.account_id in data:
+        raise HTTPException(status_code=400, detail="Data is Existed.")
+    
+    account_data = account.model_dump(exclude={"account_id"})
+
+    data[account.account_id] = account_data
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content='Data Successfully Added.')
